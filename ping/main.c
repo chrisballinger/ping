ffea00f210182f8295c3311c94c7efa30b44f165
Chris Ballinger <chrisballinger@gmail.com>
2013-01-08 23:42:07 -0800

    Initial Commit

---
+++ ping/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  ping
+//
+//  Created by Christopher Ballinger on 1/8/13.
+//  Copyright (c) 2013 ChatSecure. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
